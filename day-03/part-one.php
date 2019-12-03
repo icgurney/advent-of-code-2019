05662c0e03a4b5f1bd3aa7cf830d8cb5038843eb
Ian Gurney <ianclaytongurney@gmail.com>
2019-12-03 13:31:50 -0600

    Brute force solution. Need to refactor.

---
+++ day-03/part-one.php
@@ -0,0 +1,73 @@
+<?php
+// This works but sucks. Need to refactor.
+$start = [0,0];
+
+$input1 = file_get_contents("input1.txt");
+$input1 = explode(",", $input1);
+
+$input2 = file_get_contents("input2.txt");
+$input2 = explode(",", $input2);
+
+$pointer1 = [0,0];
+$pointer2 = [0,0];
+
+$wire1 = [];
+$wire2 = [];
+
+$crossed = [];
+
+while($input1){
+    $command1 = array_shift($input1);
+    $direction1 = substr($command1, 0, 1);
+    $distance1 = intval(substr($command1, 1));
+    for($i=0; $i<$distance1; $i++){
+        $pointer1 = move($pointer1, $direction1);
+        array_push($wire1, $pointer1);
+    }
+
+}
+
+while($input2){
+    $command2 = array_shift($input2);
+    $direction2 = substr($command2, 0, 1);
+    $distance2 = intval(substr($command2, 1));
+    for($i=0;$i<$distance2;$i++){
+        $pointer2 = move($pointer2, $direction2);
+        array_push($wire2, $pointer2);
+    }
+}
+
+foreach($wire1 as $j){
+    foreach($wire2 as $k){
+        if($j[0]==$k[0] && $j[1]==$k[1]){
+            array_push($crossed, taxiDistance($start, $j));
+        }
+    }
+}
+
+sort($crossed);
+
+print_r($crossed[0]);
+
+function taxiDistance($origin, $destination){
+    return abs($origin[0] - $destination[0]) + abs($origin[1] - $destination[1]);
+}
+
+function move($pointer, $direction){
+    switch ($direction){
+        case 'U':
+            $pointer[1]+=1;
+            break;
+        case 'R':
+            $pointer[0]+=1;
+            break;
+        case 'D':
+            $pointer[1]-=1;
+            break;
+        case 'L':
+            $pointer[0]-=1;
+            break;
+    }
+
+    return $pointer;
+}
