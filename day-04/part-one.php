2b8b1befdf202ca1110622a37d30c7a2a29d96f4
Ian Gurney <ianclaytongurney@gmail.com>
2019-12-04 10:06:24 -0600

    Part one solution

---
+++ day-04/part-one.php
@@ -0,0 +1,48 @@
+<?php
+
+$input = file_get_contents("input1.txt");
+$input = explode("-", $input);
+
+$lowerBound = $input[0];
+$upperBound = $input[1];
+
+$passwords = [];
+
+$passInt = $lowerBound;
+$passStr = strval($passInt);
+
+for($passInt = $lowerBound; $passStr <= $upperBound; $passInt++){
+    
+    $passStr = strval($passInt);
+
+    $flag = TRUE;
+
+    // It is a six-digit number.
+    if(strlen($passStr) != 6){
+        $flag = FALSE;
+    }
+    // The value is within the range given in your puzzle input.
+    if($passInt<=$lowerBound || $passInt>=$upperBound){
+        $flag = FALSE;
+    }
+    // Two adjacent digits are the same (like 22 in 122345).
+    $counter = 0;
+    for($i = 0; $i < (strlen($passStr) - 1); $i++){
+        if($passStr[$i] == $passStr[$i + 1]){
+                $counter++;
+        }
+    }
+    if($counter == 0){
+        $flag = FALSE;
+    }
+    // Going from left to right, the digits never decrease; they only ever increase or stay the same (like 111123 or 135679).
+    for($i = 0; $i < (strlen($passStr) - 1); $i++){
+        if($passStr[$i] > $passStr[$i + 1]){
+            $flag = FALSE;
+        }
+    }
+
+    if($flag) array_push($passwords, $passInt);
+}
+
+echo count($passwords) . "\n";
